06552038ac536235ad100e8653f4357b8020a817
Terry Chung <terry.chung@proagrica.com>
2025-02-06 17:30:24 +1100

    INTERLOK-4483: Updates to use interlok testing common artifacts

--- src/test/java/com/adaptris/csv/test/DefaultFunctionalTest.java
+++ src/test/java/com/adaptris/csv/test/DefaultFunctionalTest.java
@@ -1,37 +1,18 @@
 package com.adaptris.csv.test;
 
-import com.adaptris.interlok.boot.InterlokLauncher;
+import com.adaptris.testing.AbstractAdapterFunctionalTest;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.HttpEntities;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.InputStream;
 import java.util.List;
 
-
-public class DefaultFunctionalTest {
-    private static CloseableHttpClient client = HttpClients.createDefault();
-
-    @BeforeAll
-    public static void setup() throws Exception {
-        InterlokLauncher.main(new String[] {"./config/bootstrap.properties"});
-        Thread.sleep(10000); // wait for adapter to start up
-        // TODO: is there some way to hook into when the adapter has started up?
-    }
-
-    @AfterAll
-    public static void tearDown() throws Exception {
-        client.close();
-    }
-
+public class DefaultFunctionalTest extends AbstractAdapterFunctionalTest {
     static class TestCaseData {
         String url;
         String input;
@@ -44,16 +25,17 @@ public class DefaultFunctionalTest {
         }
     }
 
-    static List<TestCaseData> default_test_case_data_csv_json_xml() {
+    List<TestCaseData> default_test_case_data_csv_json_xml() {
+        String adapterBaseUrl = getBaseAdapterUrl();
         return List.of(
-            new TestCaseData("http://localhost:8081/csv-to/xml", "example-3-lines.csv", "example-3-lines.xml"),
-            new TestCaseData("http://localhost:8081/csv-to/xml", "example-586-lines.csv", "example-586-lines.xml"),
-            new TestCaseData("http://localhost:8081/streaming-csv-to/xml", "example-586-lines.csv", "example-586-lines-streaming.xml"),
-            new TestCaseData("http://localhost:8081/unchecked-csv-to/xml", "unchecked-example.csv", "unchecked-example.xml"),
-            new TestCaseData("http://localhost:8081/csv-to/json/array", "example-3-lines.csv", "example-3-records.json"),
-            new TestCaseData("http://localhost:8081/csv-to/json/lines", "example-3-lines.csv", "example-3-lines2.json"),
-            new TestCaseData("http://localhost:8081/json-to/csv", "example-3-records.json", "example-3-lines.csv"),
-            new TestCaseData("http://localhost:8081/json-to-fixed/csv?header=Priority,Issue+key,Status", "example-3-records.json", "example-3-lines-fixed.csv")
+            new TestCaseData(adapterBaseUrl + "/csv-to/xml", "example-3-lines.csv", "example-3-lines.xml"),
+            new TestCaseData(adapterBaseUrl + "/csv-to/xml", "example-586-lines.csv", "example-586-lines.xml"),
+            new TestCaseData(adapterBaseUrl + "/streaming-csv-to/xml", "example-586-lines.csv", "example-586-lines-streaming.xml"),
+            new TestCaseData(adapterBaseUrl + "/unchecked-csv-to/xml", "unchecked-example.csv", "unchecked-example.xml"),
+            new TestCaseData(adapterBaseUrl + "/csv-to/json/array", "example-3-lines.csv", "example-3-records.json"),
+            new TestCaseData(adapterBaseUrl + "/csv-to/json/lines", "example-3-lines.csv", "example-3-lines2.json"),
+            new TestCaseData(adapterBaseUrl + "/json-to/csv", "example-3-records.json", "example-3-lines.csv"),
+            new TestCaseData(adapterBaseUrl + "/json-to-fixed/csv?header=Priority,Issue+key,Status", "example-3-records.json", "example-3-lines-fixed.csv")
         );
     }
 
